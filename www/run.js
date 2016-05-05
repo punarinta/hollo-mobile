bc3b762f4d5636f80ccdafe84faee83964f122db
punarinta <cheaterx@yandex.ru>
2016-05-05 17:24:09 +0200

    nocache + connection check

--- www/run.js
+++ www/run.js
@@ -1,4 +1,7 @@
 document.addEventListener('deviceready', function ()
 {
-  window.location.href = 'https://app.hollo.email';
+  if( navigator.connection.type == 0 || navigator.connection.type=='none')
+    alert('Connect to Internet and restart the app.');
+  else
+    window.location.href = 'https://app.hollo.email/?foobar=' + Math.random();
 }, false);
