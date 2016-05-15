783d008e627318cb92a86d1ce34b1ba32522347a
punarinta <cheaterx@yandex.ru>
2016-05-15 13:38:10 +0200

    startup refactored

--- www/run.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('deviceready', function ()
-{
-  if( navigator.connection.type == 0 || navigator.connection.type=='none')
-    alert('Connect to Internet and restart the app.');
-  else
-    window.location.href = 'https://app.hollo.email/?foobar=' + Math.random();
-}, false);
